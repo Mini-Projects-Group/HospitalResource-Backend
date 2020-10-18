da07966e875394abb0ec617ab22f0b99dc16b20c
pradyumnap <pradyumnaraje.patil@gmail.com>
2020-10-18 22:28:04 +0530

    LOGIN COMPLETED

--- db/connection.js
+++ db/connection.js
@@ -12,13 +12,13 @@ const connection = mysql.createConnection({
   connection.connect(function (err) {
     if (err) throw err;
     // HOSPITAL USER
-    const hospital_user_sql = "CREATE TABLE IF NOT EXISTS hospital(hospital_id INT PRIMARY KEY AUTO_INCREMENT,hospital_name VARCHAR(255) NOT NULL,email_id VARCHAR(50) NOT NULL UNIQUE,password VARCHAR(100) NOT NULL,address VARCHAR(255) NOT NULL,contact_no BIGINT NOT NULL);";
+    const hospital_user_sql = "CREATE TABLE IF NOT EXISTS hospital(hospital_id INT PRIMARY KEY AUTO_INCREMENT,hospital_name VARCHAR(255) NOT NULL,email_id VARCHAR(50) NOT NULL UNIQUE,password VARCHAR(100) NOT NULL,address VARCHAR(255) NOT NULL,contact_no BIGINT NOT NULL,type VARCHAR(255) DEFAULT 'hospital');";
     connection.query(hospital_user_sql, (error, result) => {
       if (error) throw error;
       console.log("Hospital User Table created");
     });
     // SELLER USER
-    const seller_user_sql = "CREATE TABLE IF NOT EXISTS seller(seller_id INT PRIMARY KEY AUTO_INCREMENT,shop_name VARCHAR(255) NOT NULL UNIQUE, seller_name VARCHAR(255) NOT NULL, email_id VARCHAR(255) NOT NULL UNIQUE, password VARCHAR(100) NOT NULL, address VARCHAR(255) NOT NULL,contact_no BIGINT NOT NULL);"
+    const seller_user_sql = "CREATE TABLE IF NOT EXISTS seller(seller_id INT PRIMARY KEY AUTO_INCREMENT,shop_name VARCHAR(255) NOT NULL UNIQUE, seller_name VARCHAR(255) NOT NULL, email_id VARCHAR(255) NOT NULL UNIQUE, password VARCHAR(100) NOT NULL, address VARCHAR(255) NOT NULL,contact_no BIGINT NOT NULL,type VARCHAR(255) DEFAULT 'seller');"
     connection.query(seller_user_sql, ( error, result) => {
       if(error) throw error;
       console.log("Seller User Table Created");
