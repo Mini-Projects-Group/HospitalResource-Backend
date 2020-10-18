f311da8a82bc49f5e3e177c433be6f2c11785ab1
pradyumnap <pradyumnaraje.patil@gmail.com>
2020-10-18 19:56:37 +0530

    SIGNUP JWT DONE

--- app.js
+++ app.js
@@ -2,47 +2,30 @@ const express = require("express");
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const strategy = require('./strategy/auth');
 
 const mysql = require("mysql");
 const port = process.env.PORT || 5000;
 
+const connection = require('./db/connection');
+
 // ROUTES
 const authRoute = require('./routes/auth');
 const passport = require("passport");
 
-const connection = mysql.createConnection({
-  host: process.env.MYSQL_HOSTNAME,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT,
-});
 
-connection.connect(function (err) {
-  if (err) throw err;
-  const sql = "CREATE TABLE IF NOT EXISTS test(id int) ";
 
-  connection.query(sql, (error, result) => {
-    if (error) throw error;
-    console.log("Table created");
-  });
-
-  console.log("Connected to db");
-});
 const app = express();
 app.use(cors());
 app.use(passport.initialize());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-
-app.use( '/api/auth', authRoute);
+app.use('/api/auth', authRoute);
 
 app.get("/", (req, res) => {
-  const token = jwt.sign({
-    "name": "John Doe",
-  },process.env.JWT_SECRET);
-
-  res.send(token);
+  res.send("API for DBMS");
 });
 
 
