6e08f67ab661d3451d5f6c05b55f082300722762
Sanketmundada <mundadasanket2000@gmail.com>
2020-10-29 13:00:49 +0530

    add item done

--- routes/items.js
+++ routes/items.js
@@ -2,11 +2,26 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const verifySeller = require('../verify/verifySeller');
+const connection = require('../db/connection');
+const util = require('util');
+
+const query = util.promisify(connection.query).bind(connection);
+
 router.post("/add",
             passport.authenticate('jwt', { session : false }),
             verifySeller,
             async(req,res)=>{
-                 res.send(req.user);  
+                const {item_name,quantity,unit_price} = req.body;
+                let insertQuery;
+                insertQuery = "INSERT INTO item VALUES(DEFAULT," + req.user.seller_id + ",'" + item_name + "'," + quantity+ "," + unit_price + ");";
+                try{
+                    connection.query(insertQuery,async (err , result) => {
+                        if(err) res.status(400).json({"message" : "Error in adding Item","error" : true});
+                        else res.status(200).json({"message":"Item Added Successfully","error":false});
+                    });
+                }catch(error){
+                    return res.status(400).json({"message" : "Error","error" : true});
+                }
             }
         );
 
