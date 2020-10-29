eb690d90421f79a7762f875b9e5b97efc3ab4bd0
Nimish Dadlani <nmd2611@gmail.com>
2020-10-29 13:16:25 +0530

    SOme profress

--- db/connection.js
+++ db/connection.js
@@ -2,29 +2,41 @@ const mysql = require("mysql");
 const express = require("express");
 
 const connection = mysql.createConnection({
-    host: process.env.MYSQL_HOSTNAME,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    port: process.env.MYSQL_PORT,
+  host: process.env.MYSQL_HOSTNAME,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: process.env.MYSQL_PORT,
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  // HOSPITAL USER
+  const hospital_user_sql =
+    "CREATE TABLE IF NOT EXISTS hospital(hospital_id INT PRIMARY KEY AUTO_INCREMENT,hospital_name VARCHAR(255) NOT NULL,email_id VARCHAR(50) NOT NULL UNIQUE,password VARCHAR(100) NOT NULL,address VARCHAR(255) NOT NULL,contact_no BIGINT NOT NULL,type VARCHAR(255) DEFAULT 'hospital');";
+  connection.query(hospital_user_sql, (error, result) => {
+    if (error) throw error;
+    console.log("Hospital User Table created");
   });
-  
-  connection.connect(function (err) {
-    if (err) throw err;
-    // HOSPITAL USER
-    const hospital_user_sql = "CREATE TABLE IF NOT EXISTS hospital(hospital_id INT PRIMARY KEY AUTO_INCREMENT,hospital_name VARCHAR(255) NOT NULL,email_id VARCHAR(50) NOT NULL UNIQUE,password VARCHAR(100) NOT NULL,address VARCHAR(255) NOT NULL,contact_no BIGINT NOT NULL,type VARCHAR(255) DEFAULT 'hospital');";
-    connection.query(hospital_user_sql, (error, result) => {
-      if (error) throw error;
-      console.log("Hospital User Table created");
-    });
-    // SELLER USER
-    const seller_user_sql = "CREATE TABLE IF NOT EXISTS seller(seller_id INT PRIMARY KEY AUTO_INCREMENT,shop_name VARCHAR(255) NOT NULL UNIQUE, seller_name VARCHAR(255) NOT NULL, email_id VARCHAR(255) NOT NULL UNIQUE, password VARCHAR(100) NOT NULL, address VARCHAR(255) NOT NULL,contact_no BIGINT NOT NULL,type VARCHAR(255) DEFAULT 'seller');"
-    connection.query(seller_user_sql, ( error, result) => {
-      if(error) throw error;
-      console.log("Seller User Table Created");
-    });
-  
-    console.log("Connected to db");
+  // SELLER USER
+  const seller_user_sql =
+    "CREATE TABLE IF NOT EXISTS seller(seller_id INT PRIMARY KEY AUTO_INCREMENT,shop_name VARCHAR(255) NOT NULL UNIQUE, seller_name VARCHAR(255) NOT NULL, email_id VARCHAR(255) NOT NULL UNIQUE, password VARCHAR(100) NOT NULL, address VARCHAR(255) NOT NULL,contact_no BIGINT NOT NULL,type VARCHAR(255) DEFAULT 'seller');";
+  connection.query(seller_user_sql, (error, result) => {
+    if (error) throw error;
+    console.log("Seller User Table Created");
   });
+
+  // ORDERS TABLE
+  const orders_sql =
+    "CREATE TABLE IF NOT EXISTS orders(order_id INT PRIMARY KEY AUTO_INCREMENT, seller_id INT NOT NULL, hospital_id INT NOT NULL, items JSON ,date_order DATE, date_delivery DATE, FOREIGN KEY (seller_id) REFERENCES seller(seller_id) ON DELETE CASCADE ON UPDATE CASCADE,FOREIGN KEY (hospital_id) REFERENCES hospital(hospital_id) ON DELETE CASCADE ON UPDATE CASCADE);";
+
+  connection.query(orders_sql, (error, result) => {
+    if (error) throw error;
+
+    console.log("Orders table created");
+  });
+
+  console.log("Connected to db");
+});
 
 module.exports = connection;
--- routes/orders.js
+++ routes/orders.js
@@ -1,4 +1,12 @@
 const express = require("express");
+const passport = require("passport");
+const connection = require("../db/connection");
+const util = require("util");
+
+const query = util.promisify(connection.query).bind(connection);
+
 const router = express.Router();
 
-router.get();
+//router.get();
+
+module.exports = router;
