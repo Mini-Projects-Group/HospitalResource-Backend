3e10f18bfe160ba8e5cf44b3106d0232dc6ac577
Sanketmundada <mundadasanket2000@gmail.com>
2020-10-29 13:56:05 +0530

    Delete Item part done

--- routes/items.js
+++ routes/items.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const verifySeller = require('../verify/verifySeller');
 const connection = require('../db/connection');
 const util = require('util');
 
+const verifySeller = require('../verify/verifySeller');
+const verifyItemSeller = require('../verify/verifyItemSeller');
+
 const query = util.promisify(connection.query).bind(connection);
 
+//ADD ITEM
 router.post("/add",
             passport.authenticate('jwt', { session : false }),
             verifySeller,
@@ -24,5 +27,25 @@ router.post("/add",
                 }
             }
         );
+  
+        
+//DELETE ITEM
+router.delete("/delete", 
+                passport.authenticate('jwt', { session : false }),
+                verifySeller,
+                verifyItemSeller,
+                async(req,res)=>{
+                    try{
+                        let deleteQuery= "DELETE FROM item WHERE item_id =" + req.body.item_id + ";";
+                        connection.query(deleteQuery, async (err, result) => {
+                            if (err) res.status(400).json({"message" : "Error in deleting Item","error" : true});
+                            else res.status(200).json({"message":"Item Deleted Successfully","error":false});
+                        });
+                    }catch(error){
+                        console.log(error);
+                        return res.status(400).json({"message" : "Error","error" : true});
+                    }
+                }
+            )
 
 module.exports = router;
