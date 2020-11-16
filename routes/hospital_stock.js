677a1cdc5eb32424f0d434315bd28f7400ad2b27
pradyumnap <pradyumnaraje.patil@gmail.com>
2020-11-16 10:34:16 +0530

    DELETE ORDER ROUTE ADDED

--- routes/hospital_stock.js
+++ routes/hospital_stock.js
@@ -6,92 +6,89 @@ const verifyHospital = require('../verify/verifyHospital');
 
 const router = express.Router();
 
-
-
 router.get('/approve/:order_id',
-    passport.authenticate('jwt',{ session : false}),
+    passport.authenticate('jwt', { session: false }),
     verifySellerApprovingOrder,
-    async( req , res ) => {
+    async(req, res) => {
 
-    let user_id = req.user.seller_id;
-    let order_id = req.params.order_id;
+        let user_id = req.user.seller_id;
+        let order_id = req.params.order_id;
 
-    let current_order = await query(`SELECT * FROM orders WHERE order_id=${order_id} AND seller_id=${user_id};`);
-    let hospital_stock = await query(`SELECT * FROM hospital_stock WHERE hospital_id=${current_order[0].hospital_id};`);
-    let seller_list = await query(`SELECT * FROM item WHERE seller_id=${user_id};`)
-
-
-    let hospital_items_list = JSON.parse(hospital_stock[0].items);
-    let item_list = JSON.parse(current_order[0].items);
+        let current_order = await query(`SELECT * FROM orders WHERE order_id=${order_id} AND seller_id=${user_id};`);
+        let hospital_stock = await query(`SELECT * FROM hospital_stock WHERE hospital_id=${current_order[0].hospital_id};`);
+        let seller_list = await query(`SELECT * FROM item WHERE seller_id=${user_id};`)
 
 
-    let canFulfill = 1;
-    for(i = 0 ; i < item_list.length ; i++ ) {
-        let flag = false;
-        for(j = 0 ; j < seller_list.length ; j++ ) {
-            if( seller_list[j].item_name.replace(/\s/g,'') == item_list[i].item.replace(/\s/g,'') &&
-                 seller_list[j].quantity >= item_list[i].quantity) {
-                // console.log(i,j,item_list[i],seller_list[j]);
-                flag = true;
+        let hospital_items_list = JSON.parse(hospital_stock[0].items);
+        let item_list = JSON.parse(current_order[0].items);
+
+
+        let canFulfill = 1;
+        for (i = 0; i < item_list.length; i++) {
+            let flag = false;
+            for (j = 0; j < seller_list.length; j++) {
+                if (seller_list[j].item_name.replace(/\s/g, '') == item_list[i].item.replace(/\s/g, '') &&
+                    seller_list[j].quantity >= item_list[i].quantity) {
+                    // console.log(i,j,item_list[i],seller_list[j]);
+                    flag = true;
+                }
+            }
+            if (flag == false) canFulfill = 0;
+        }
+        if (canFulfill == 0) {
+            return res.json({ error: true, message: "Seller does not have sufficient items." })
+        } else {
+            // UPDATE THE SELLER TABLE
+            for (i = 0; i < item_list.length; i++) {
+                let updateQuery =
+                    "UPDATE item SET quantity=quantity-" + item_list[i].quantity + " WHERE seller_id=" + user_id + " AND item_name='" + item_list[i].item + "';";
+                let result = query(updateQuery);
             }
         }
-        if(flag == false) canFulfill = 0;
-    }
-    if(canFulfill == 0) {
-        return res.json({error: true,message: "Seller does not have sufficient items." })
-    } else {
-        // UPDATE THE SELLER TABLE
-        for(i = 0 ; i < item_list.length ; i++ ) {
-            let updateQuery = 
-            "UPDATE item SET quantity=quantity-" + item_list[i].quantity + " WHERE seller_id=" + user_id + " AND item_name='" + item_list[i].item + "';";
-            let result = query(updateQuery);
-        }
-    }
 
-    for(i = 0 ; i < item_list.length ; i++ ) {
-        let flag = false;
-        for(j = 0 ; j < hospital_items_list.length ; j++ ) {
-            if( hospital_items_list[j].item.replace(/\s/g,'') == item_list[i].item.replace(/\s/g,'')) {
-                flag = true;
-                hospital_items_list[j].quantity += item_list[i].quantity;
+        for (i = 0; i < item_list.length; i++) {
+            let flag = false;
+            for (j = 0; j < hospital_items_list.length; j++) {
+                if (hospital_items_list[j].item.replace(/\s/g, '') == item_list[i].item.replace(/\s/g, '')) {
+                    flag = true;
+                    hospital_items_list[j].quantity += item_list[i].quantity;
+                }
             }
+            if (flag == false) hospital_items_list.push({ item: item_list[i].item, quantity: item_list[i].quantity });
         }
-        if(flag == false) hospital_items_list.push({item: item_list[i].item,quantity: item_list[i].quantity});
-    }
 
-    let updated_hospital_stock = 
-    `UPDATE hospital_stock SET items='${JSON.stringify(hospital_items_list)}' WHERE hospital_id=${current_order[0].hospital_id};`;
-    
-    let result = await query(updated_hospital_stock);
-    
-    // CHANGING THE STATUS TO DELIVERED
-    let updateQuery = 
-    `UPDATE orders SET status='delivered', date_delivery=NOW() WHERE order_id=${order_id} AND seller_id=${user_id};`;
-    result = await query(updateQuery);
+        let updated_hospital_stock =
+            `UPDATE hospital_stock SET items='${JSON.stringify(hospital_items_list)}' WHERE hospital_id=${current_order[0].hospital_id};`;
+
+        let result = await query(updated_hospital_stock);
+
+        // CHANGING THE STATUS TO DELIVERED
+        let updateQuery =
+            `UPDATE orders SET status='delivered', date_delivery=NOW() WHERE order_id=${order_id} AND seller_id=${user_id};`;
+        result = await query(updateQuery);
 
 
-        return res.json(
-            {
-                error:false,
-                message:"Approved"
-            });
+        return res.json({
+            error: false,
+            message: "Approved"
+        });
     }
 );
 
 
 router.get('/',
-    passport.authenticate('jwt',{session: false}),
+    passport.authenticate('jwt', { session: false }),
     verifyHospital,
-    async (req, res) => {
+    async(req, res) => {
         try {
             let hospital_id = req.user.hospital_id;
             console.log(hospital_id);
 
             // GET THE STOCK AND ORDERS OF HOSPITAL LOGGED IN
-            let hospital_orders_query = 
-            `SELECT * FROM orders WHERE hospital_id=${hospital_id};`;
-            let hospital_stock_query = 
-            `SELECT * FROM hospital NATURAL JOIN hospital_stock WHERE hospital.hospital_id=${hospital_id};`;
+            let hospital_orders_query =
+                `SELECT * FROM orders WHERE hospital_id=${hospital_id};`;
+            let hospital_stock_query =
+                `SELECT * FROM hospital NATURAL JOIN hospital_stock WHERE hospital.hospital_id=${hospital_id};`;
 
             let orders = await query(hospital_orders_query);
             let stock = await query(hospital_stock_query);
@@ -100,52 +97,47 @@ router.get('/',
 
             let items = JSON.parse(stock[0].items);
             let total = 0;
-            for(i = 0 ; i < items.length ; i++ ) {
+            for (i = 0; i < items.length; i++) {
                 total += items[i].quantity;
             }
-            for(i = 0 ; i < items.length ; i++ ) {
-                stockPercentage.push(
-                    {
-                        'item_name':items[i].item,
-                        'percent': parseFloat((items[i].quantity/total) * 100 ).toFixed(2)
-                    }
-                );
+            for (i = 0; i < items.length; i++) {
+                stockPercentage.push({
+                    'item_name': items[i].item,
+                    'percent': parseFloat((items[i].quantity / total) * 100).toFixed(2)
+                });
             }
 
             let stockPercentage_used = [];
             let items_used = JSON.parse(stock[0].items_used);
             total = 0;
-            for(i = 0 ; i < items_used.length ; i++ ) {
+            for (i = 0; i < items_used.length; i++) {
                 total += items_used[i].quantity;
             }
-            for(i = 0 ; i < items_used.length ; i++ ) {
-                stockPercentage_used.push(
-                    {
-                        'item_name': items_used[i].item,
-                        'percent': parseFloat((items_used[i].quantity/total)*100).toFixed(2)
-                    }
-                );
+            for (i = 0; i < items_used.length; i++) {
+                stockPercentage_used.push({
+                    'item_name': items_used[i].item,
+                    'percent': parseFloat((items_used[i].quantity / total) * 100).toFixed(2)
+                });
             }
 
-            return res.json(
-                {
-                    error: false,
-                    message: 'Hospital Info Delivered',
-                    orders: orders,stock: stockPercentage,
-                    stock_used: stockPercentage_used
-                });
-        } catch(e) {
+            return res.json({
+                error: false,
+                message: 'Hospital Info Delivered',
+                orders: orders,
+                stock: stockPercentage,
+                stock_used: stockPercentage_used
+            });
+        } catch (e) {
             console.log(e);
-            return res.json(
-                {
-                    error: true,
-                    message: 'Unknown Error Occurred',
-                    orders: [],
-                    stock: [],
-                    stock_used: []
-                });
+            return res.json({
+                error: true,
+                message: 'Unknown Error Occurred',
+                orders: [],
+                stock: [],
+                stock_used: []
+            });
         }
-});
+    });
 
 
 // INPUT FORMAT
@@ -161,9 +153,9 @@ router.get('/',
 //     ]
 // }
 router.post('/used',
-    passport.authenticate('jwt',{session: false}),
+    passport.authenticate('jwt', { session: false }),
     verifyHospital,
-    async (req, res) => {
+    async(req, res) => {
         try {
             // req.body.items is array of json object {item: value,quantity: value}
             let items = req.body.items;
@@ -172,13 +164,13 @@ router.post('/used',
             let stock_items = JSON.parse(stock[0].items);
             let stock_used_items = JSON.parse(stock[0].items_used);
 
-            for(i = 0 ; i < items.length ; i++ ) {
+            for (i = 0; i < items.length; i++) {
 
                 // UPDATE THE LIST STOCK_ITEMS
-                for(j = 0 ; j < stock_items.length ; j++ ) {
-                    if(items[i].item.replace(/\s/g,'') == stock_items[j].item.replace(/\s/g,'')) {
-                        if( items[i].quantity > stock_items[j].quantity ) {
-                            return res.json({error: true, message: `You don't have sufficient ${items[i].item}`});
+                for (j = 0; j < stock_items.length; j++) {
+                    if (items[i].item.replace(/\s/g, '') == stock_items[j].item.replace(/\s/g, '')) {
+                        if (items[i].quantity > stock_items[j].quantity) {
+                            return res.json({ error: true, message: `You don't have sufficient ${items[i].item}` });
                         } else {
                             stock_items[j].quantity -= items[i].quantity;
                             break;
@@ -187,18 +179,18 @@ router.post('/used',
                 }
                 let flag = false;
                 // UPDATE THE LIST STOCK_USED_ITEMS
-                for(j = 0 ; j < stock_used_items.length ; j++ ) {
-                    if(items[i].item.replace(/\s/g,'') == stock_used_items[j].item.replace(/\s/g,'')) {
+                for (j = 0; j < stock_used_items.length; j++) {
+                    if (items[i].item.replace(/\s/g, '') == stock_used_items[j].item.replace(/\s/g, '')) {
                         stock_used_items[j].quantity += items[i].quantity;
                         flag = true;
                         break;
                     }
                 }
-                if( !flag ) stock_used_items.push(items[i]);
+                if (!flag) stock_used_items.push(items[i]);
             }
 
-            let update_stock_table = 
-            `UPDATE hospital_stock SET items='${JSON.stringify(stock_items)}', items_used='${JSON.stringify(stock_used_items)}' WHERE hospital_id=${req.user.hospital_id};`;
+            let update_stock_table =
+                `UPDATE hospital_stock SET items='${JSON.stringify(stock_items)}', items_used='${JSON.stringify(stock_used_items)}' WHERE hospital_id=${req.user.hospital_id};`;
 
             let result = await query(update_stock_table);
 
@@ -207,15 +199,14 @@ router.post('/used',
                 message: 'Items Added to Used'
             });
 
-        } catch(e) {
+        } catch (e) {
             console.log(e);
-            return res.json(
-                {
-                    error: true,
-                    message: 'Unknown Error Occurred',
-                });
+            return res.json({
+                error: true,
+                message: 'Unknown Error Occurred',
+            });
         }
-});
+    });
 
 
 module.exports = router;
--- routes/orders.js
+++ routes/orders.js
@@ -2,49 +2,76 @@ const express = require("express");
 const passport = require("passport");
 const connection = require("../db/connection");
 const util = require("util");
+const verifySellerApprovingOrder = require('../verify/verifySellerApprovingOrder');
+const query = require('../shared/queryPromise');
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  res.send("Orders api");
+router.get("/", async(req, res) => {
+    res.send("Orders api");
 });
 
 router.post(
-  "/add",
-  passport.authenticate("jwt", { session: false }),
-  async (req, res) => {
-    const { seller_id,hospital_id, items } = req.body;
-    console.log(req.user);
-    const status = "pending"; // status will be pending by default
+    "/add",
+    passport.authenticate("jwt", { session: false }),
+    async(req, res) => {
+        const { seller_id, hospital_id, items } = req.body;
+        console.log(req.user);
+        const status = "pending"; // status will be pending by default
 
-    let insertQuery =
-      "INSERT INTO orders VALUES(NULL," +
-      req.body.seller_id +
-      "," +
-      req.user.hospital_id +
-      ", '" +
-      JSON.stringify(items) + // array of items
-      "' , '" +
-      status +
-      "', CURDATE() , NULL" + // date of order
-      " );";
+        let insertQuery =
+            "INSERT INTO orders VALUES(NULL," +
+            req.body.seller_id +
+            "," +
+            req.user.hospital_id +
+            ", '" +
+            JSON.stringify(items) + // array of items
+            "' , '" +
+            status +
+            "', CURDATE() , NULL" + // date of order
+            " );";
 
-    try {
-      connection.query(insertQuery, async (error, result) => {
-        if (error) {
-          console.log(error);
-          res
-            .status(400)
-            .json({ message: "Error in placing error", error: true });
-        } else
-          res
-            .status(200)
-            .json({ message: "Order placed successfully!!", error: false });
-      });
-    } catch (error) {
-      return res.status(400).json({ message: "Error", error: true });
+        try {
+            connection.query(insertQuery, async(error, result) => {
+                if (error) {
+                    console.log(error);
+                    res
+                        .status(400)
+                        .json({ message: "Error in placing error", error: true });
+                } else
+                    res
+                    .status(200)
+                    .json({ message: "Order placed successfully!!", error: false });
+            });
+        } catch (error) {
+            return res.status(400).json({ message: "Error", error: true });
+        }
     }
-  }
 );
+
+
+
+router.delete('/:order_id',
+    passport.authenticate('jwt', { session: false }),
+    verifySellerApprovingOrder,
+    async(req, res) => {
+        try {
+            let user_id = req.user.seller_id;
+            let order_id = req.params.order_id;
+
+            let current_order_delete_query = `DELETE FROM orders WHERE order_id=${order_id} AND seller_id=${user_id};`;
+            let result = await query(current_order_delete_query);
+            return res.json({
+                error: false,
+                message: "Deleted"
+            });
+        } catch (e) {
+            return res.json({
+                error: true,
+                message: "Error Occured"
+            });
+        }
+    })
+
 
 module.exports = router;
